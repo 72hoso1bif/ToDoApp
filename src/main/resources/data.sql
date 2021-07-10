606cebe7333884ce150cb0707e8e28f0df1603a8
IzanX <izankzot@gmail.com>
2021-07-10 23:30:59 +0200

    - updated hide delete button for non editable rows

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO roles(id, name) VALUES (1,'ROLE_USER');
+INSERT INTO roles(id, name) VALUES (2,'ROLE_MOD');
+INSERT INTO roles(id, name) VALUES (3,'ROLE_ADMIN');
+INSERT INTO users VALUES (1, 'admin@admin.de','$2a$10$GGQLeMo3ZkybPRyi17eNzOCDsnNLjATIp2Orwfb6nalFuOqXjvffi', 'admin', null);
